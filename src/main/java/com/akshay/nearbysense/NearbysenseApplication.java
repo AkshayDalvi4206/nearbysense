e2a7c16e2f34d1cdbdc9d39508bb9505968ef047
AkshayDalvi4206 <akshaydalvim@gmail.com>
2026-02-03 15:54:29 +0530

    chore: initialize Spring Boot project with base setup

---
+++ src/main/java/com/akshay/nearbysense/NearbysenseApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.nearbysense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbysenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbysenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshay/nearbysense/NearbysenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.nearbysense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearbysenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
